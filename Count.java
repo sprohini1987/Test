eab5e2b5d6df9ee9f18d487aa1091390679919b4
sprohini1987 <spshalu.1987@gmail.com>
2017-01-05 02:20:04 +0530

    Add files via upload

---
+++ Count.java
@@ -0,0 +1,15 @@
+package com.payilagam.test;
+
+public class Count {
+
+	public static void main(String[] args) {
+            for(int i=1;i<=10;i++)
+            {
+            	if(i==3){
+            		continue;
+            	}
+            	System.out.println(i);
+            }
+	}
+
+}
