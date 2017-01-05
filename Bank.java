82a5313338d66ef96574cce3a43d8310f84d43f0
sprohini1987 <spshalu.1987@gmail.com>
2017-01-06 01:18:23 +0530

    Add files via upload

--- Bank.java
+++ Bank.java
@@ -6,6 +6,7 @@ public abstract class Bank {
 		Customer a=new Customer();
 		a.getcustomerdetails();
 		a.getloan();
+		a.giveloanforCar();
 
 	}
 
--- Customer.java
+++ Customer.java
@@ -8,9 +8,18 @@ public class Customer extends Bank {
 			System.out.println("Mob No:8972468902");
 		}
 
-		public void getloan() {
+		protected void getloan() {
 		int interest=(((200000*12)/100)/36);
 			System.out.println("Interest for loan="+interest);
 			
+		}
+		private void getCar(){
+			System.out.println("Buy car this year");
+		}
+
+		 void giveloanforCar() {
+			// TODO Auto-generated method stub
+			int carinterest=((150000*9)/100)/24;
+			System.out.println("interest for car loan= "+carinterest);
 		}
 }
